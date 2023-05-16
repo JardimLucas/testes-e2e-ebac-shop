b7ff998044216651ba98b24a3c2cefc0acd0fa32
Lucas Jardim <lucasdovalle@hotmail.com>
2023-05-16 09:11:50 -0300

    Inlcusão do Jenkinsfile e correções

--- cypress/e2e/e2e.cy.js
+++ cypress/e2e/e2e.cy.js
@@ -23,13 +23,14 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
         //Escolhendo os produtos:
         cy.addProdutos('Abominable Hoodie', 'XS', 'Blue', 2)
         cy.get('#primary-menu > .menu-item-629 > a').click()
+        cy.get(':nth-child(2) > .page-numbers').click()
         cy.addProdutos('Atlas Fitness Tank', 'XL', 'Blue', 5)
         cy.get('#primary-menu > .menu-item-629 > a').click()
         cy.get(':nth-child(2) > .page-numbers').click()
         cy.addProdutos('Atomic Endurance Running Tee (Crew-Neck)', 'XL', 'Blue', 3)
         cy.get('#primary-menu > .menu-item-629 > a').click()
         cy.get(':nth-child(2) > .page-numbers').click()
-        cy.addProdutos('Balboa Persistence Tee', 'XL', 'Green', 7)
+        cy.addProdutos('Balboa Persistence Tee', 'XS', 'Green', 1)
 
 
         //Abrindo o carrinho de compras para pagamento
